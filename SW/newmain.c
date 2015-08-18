f78b650ead14e793ab1f256fc5a5918de88ba356
heresiarch <heresiarch@saturn>
2015-08-18 11:28:53 +0200

    initial

---
+++ SW/newmain.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   newmain.c
+ * Author: rene
+ *
+ * Created on 18. August 2015, 11:23
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
